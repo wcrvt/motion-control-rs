e64059cc21852aadd09cd887a789965cf57b4c8b
wcrvt <cat0127.k@gmail.com>
2024-02-16 18:19:30 +0900

    rename a function in combinatorics lib

--- lib/digitalservo/src/combinatorics/combination.rs
+++ lib/digitalservo/src/combinatorics/combination.rs
@@ -3,7 +3,7 @@ use super::*;
 pub struct Combination <const N: usize, const R: usize>
 where [(); combination(N, R)]:
 {
-    pub pair: [[usize; R]; combination(N, R)],
+    pub combination: [[usize; R]; combination(N, R)],
     pub cnt: usize,
 }
 
@@ -12,30 +12,30 @@ where [(); combination(N, R)]:
 {
     pub fn new() -> Self {
         Self {
-            pair: [[0; R]; combination(N, R)],
+            combination: [[0; R]; combination(N, R)],
             cnt : 0,
         }
     }
 
-    pub fn get_pair(&mut self) {
+    pub fn get_combination(&mut self) {
         self.search_index(0, 0, [0; R]);
     }
 
-    fn search_index(&mut self, layer: usize, val: usize, mut pair: [usize; R]) {
+    fn search_index(&mut self, layer: usize, val: usize, mut combination: [usize; R]) {
         if val < N {
             if layer == 0 {
                 for i in 0..N {
-                    self.search_index(layer + 1, i, pair);
+                    self.search_index(layer + 1, i, combination);
                 }
             }
             else if layer == R {
-                pair[layer - 1] = val;
-                self.pair[self.cnt] = pair;
+                combination[layer - 1] = val;
+                self.combination[self.cnt] = combination;
                 self.cnt += 1;
             } else {
                 for i in (val + 1)..N {
-                    pair[layer - 1] = val;
-                    self.search_index(layer + 1, i, pair);
+                    combination[layer - 1] = val;
+                    self.search_index(layer + 1, i, combination);
                 }
             }
         }
@@ -45,6 +45,6 @@ where [(); combination(N, R)]:
 
 pub fn generate_combination_index<const N: usize, const R: usize> () -> [[usize; R]; combination(N, R)] {
     let mut combi: Combination<N, R> = Combination::<N, R>::new();
-    combi.get_pair();
-    combi.pair
+    combi.get_combination();
+    combi.combination
 }
